b55929dd24e345792a2215b4530a41e5f14747ad
ayobamy <ayobamyahmed@gmail.com>
2022-08-25 12:57:34 -0700

    [fix]: fixed betty style error

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,24 +5,23 @@
  * @parent: pointer to the parent of the node to create
  * @value: The value to input in the new node
  *
- * Return: a pointer to the new node;
- *         Otherwise - NULL on  failure
+ * Return: a pointer to the new node, Otherwise - NULL on  failure
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode;
+	binary_tree_t *newNode;
 
-    newNode = malloc(sizeof(binary_tree_t));
-    if (newNode == NULL)
-    {
-        return (NULL);
-    }
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
 
-    newNode->n = value;
-    newNode->parent = parent;
-    newNode->left = NULL;
-    newNode->right = NULL;
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
-    return (newNode);
+	return (newNode);
 }
